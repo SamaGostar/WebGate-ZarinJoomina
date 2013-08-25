1a5378572db3e0df30a2e788f4c8679e45974f9b
Masoud A <amini141@gmail.com>
2013-08-25 12:01:26 +0430

    Update zarin_request.php

--- plg_content_zarinjoomina/site/zarinjoomina/zarin_request.php
+++ plg_content_zarinjoomina/site/zarinjoomina/zarin_request.php
@@ -19,7 +19,11 @@
 		)
 	);
 	
-	//Redirect to URL You can do it also by creating a form
-	Header('Location: https://www.zarinpal.com/pg/StartPay/' . $res->Authority . '/');
-
+	if($res['Status']==100)
+	{
+		
+	Header('Location: https://www.zarinpal.com/pg/StartPay/' . $res->Authority);
+	}else{
+		echo'ERR: '.$res['Status'] ;
+	}
 ?>
